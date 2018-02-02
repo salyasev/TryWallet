2665da185041347532705a58397804388431dbbb
Sergey Alyasev <a_ser@bk.ru>
2018-02-02 12:09:49 +0300

    Add project

---
+++ TryWallet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TryWallet
+//
+//  Created by Sergey Alyasev on 02/02/2018.
+//  Copyright © 2018 Convergent Media Group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
